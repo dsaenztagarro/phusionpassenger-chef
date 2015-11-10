4a3926869375e3b0451396d328e7cc8eb15294ec
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-11-11 00:36:01 +0100

    Updated virtualhost resource

--- resources/virtualhost.rb
+++ resources/virtualhost.rb
@@ -4,10 +4,14 @@ property :application_path, String, default: ''
 action :enable do
   template "/etc/apache2/sites-available/#{server_name}.conf" do
     source 'virtualhost.conf.erb'
-    variables { server_name: server_name, application_path: application_path }
+    variables :server_name => server_name, :application_path => application_path
   end
 
   execute 'enable_site' do
     command "a2ensite #{server_name}"
+  end
+
+  execute 'apache_restart' do
+    command 'apachectl restart'
   end
 end
