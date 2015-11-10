1f9c3f2a51faa7291d5c0fd5582189969057a236
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-11-10 03:06:22 +0100

    Complete default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-apt_get 'apt_get_update' do
+apt_get 'update' do
   action :update
 end
 
@@ -19,15 +19,22 @@ apt_key 'apt_key_adv' do
   recv_keys node['phusionpassenger']['recv_keys']
 end
 
-apt_get 'apt_get_install' do
-  action :install
-  packages %w(apt-transport-https ca-certificates)
-end
+package %w(apt-transport-https ca-certificates)
 
 execute 'add_apt_repository' do
   command "sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main > /etc/apt/sources.list.d/passenger.list'"
 end
 
-apt_get 'apt_get_update' do
+apt_get 'update' do
   action :update
+end
+
+package 'libapache2-mod-passenger'
+
+execute 'enable_passenger_apache_module' do
+  command 'a2enmod passenger'
+end
+
+execute 'restart_apache' do
+  command 'apachectl restart'
 end
