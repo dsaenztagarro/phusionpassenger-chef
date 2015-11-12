1c637266b4bb94b68731977a090745dd6e112c1e
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-11-13 00:59:06 +0100

    Removed comment

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -8,10 +8,6 @@ describe 'phusionpassenger::default' do
     end.converge described_recipe
   end
 
-  # before(:each) do
-  #   stub_command('ls /var/lib/postgresql/9.3/main/recovery.conf').and_return(true)
-  # end
-
   it 'converges successfully' do
     expect { :chef_run }.to_not raise_error
   end
