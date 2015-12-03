33395cfb488194ac6089b95b00f0f0b941c30223
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-12-03 09:38:17 +0100

    Added metadata info

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,10 @@ license          'All rights reserved'
 description      'Installs/Configures phusionpassenger'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+recipe 'phusionpassenger::default', 'Installs Phusion Passenger'
+
+%w(debian ubuntu).each { |os| supports os }
+
+source_url 'https://github.com/dsaenztagarro/phusionpassenger' if respond_to?(:source_url)
+issues_url 'https://github.com/dsaenztagarro/phusionpassenger/issues' if respond_to?(:issues_url)
