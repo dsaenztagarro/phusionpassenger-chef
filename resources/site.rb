7368cdedf0dfc8256f75b1cc949ec74fc2a4ce4d
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-12-10 12:23:01 +0100

    Fixed permissions of site log

--- resources/site.rb
+++ resources/site.rb
@@ -6,25 +6,38 @@ property :document_root, String
 property :environment, Hash, required: true
 property :server, String, required: true
 property :user, String, required: true
+property :friendly_error_pages, kind_of: [TrueClass, FalseClass], default: false
 
 action :create do
   cmd = login_shell('passenger-config about ruby-command',
                     user: user, environment: environment)
-  ruby_command = cmd.stdout.match(/Command: (.*)\n/i).captures.first
+  ruby = cmd.stdout.match(/Command: (.*)\n/i).captures.first
+  application = server.gsub '.', '_'
 
-  template "/etc/apache2/sites-available/#{server_name}.conf" do
+  %W(/var/log/apache2/#{application}_error.log
+     /var/log/apache2/#{application}_access.log).each do |filepath|
+    file filepath do
+      owner user
+      group 'www-data'
+      action :touch
+    end
+  end
+
+  template "/etc/apache2/sites-available/#{server}.conf" do
     source 'virtualhost.conf.erb'
     cookbook 'phusionpassenger'
     variables(
-      server_name: server_name,
-      document_root: document_root || "/var/www/#{server_name}/public",
-      ruby_command: ruby_command
+      application: application,
+      document_root: document_root || "/var/www/#{server}/current/public",
+      friendly_error_pages: friendly_error_pages ? 'on' : 'off',
+      ruby: ruby,
+      server: server
     )
   end
 end
 
 action :enable do
   execute 'enabling_site' do
-    command "a2ensite #{server_name}"
+    command "a2ensite #{server}"
   end
 end
